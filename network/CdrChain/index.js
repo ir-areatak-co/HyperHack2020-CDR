725249a6a0783fbb974a4298c7af88319f0fce56
hnamazian <namazian87@gmail.com>
2020-02-27 19:36:08 +0330

    Update index file

--- network/CdrChain/index.js
+++ network/CdrChain/index.js
@@ -1,11 +1,13 @@
 const CA = require('./CA')
 const SecureGateway = require('./Gateway')
 const SecureWallet = require('./Wallet')
+const Contract = require('./Contract')
 const Network = require('./Network')
 
 module.exports = {
   CA,
   SecureGateway,
   SecureWallet,
+  Contract,
   Network
 }
