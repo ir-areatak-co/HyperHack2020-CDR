2a42051e1f9e874b07024c70e5a176adb79c055d
hnamazian <namazian87@gmail.com>
2020-02-27 21:24:51 +0330

    Add CdrContract class

--- network/CdrChain/Contract.js
+++ network/CdrChain/Contract.js
@@ -4,21 +4,42 @@ const config = require('config')
 
 class CdrContract {
   constructor (chaincodeName, channelName) {
+    this.chaincodeName = chaincodeName
+    this.channelName = channelName
+  }
+
+  async getContract () {
     const walletId = config.get('wallet').users.serverAdmin.walletId
     const walletPwd = config.get('wallet').users.serverAdmin.password
 
     const cdrGateway = new CdrGateway()
-    cdrGateway.createGatewayForUser(walletId, walletPwd, 'Org1MSP')
-      .then(gateway => {
-        this.hlfContract = new Hlf.Contract(chaincodeName, channelName, gateway)
-      })
-      .catch(ex => {
-        throw new Error(ex)
-      })
+    const gateway = await cdrGateway.createGatewayForUser(walletId, walletPwd, 'Org1MSP')
+    // const channel = await gateway.getNetwork('mychannel')
+    // const contract = channel.getContract('profile')
+
+    const client = await gateway.getClient()
+    const channel = client.getChannel('mychannel')
+    return channel
   }
 
-  async getContractEvents (eventListenerName, eventName, handler, options) {
-    await this.hlfContract.getContractEvents(eventListenerName, eventName, handler, options)
+  async getContractEvents (eventListenerName, eventName, handler, errorHandler) {
+    const channel = await this.getContract()
+    console.log(channel)
+
+    const peers = channel.getPeers()
+    const channelEventHub = channel.getChannelEventHub(peers[0].getPeer().getName())
+
+    await channelEventHub.registerChaincodeEvent(this.chaincodeName, eventName, handler, errorHandler, {
+      replay: true,
+      startBlock: 0
+    })
+
+    channelEventHub.connect(true, (error, channelEventHub) => {
+      console.log(error)
+    })
+    channelEventHub.checkConnection(true)
+
+    // await contract.addContractListener(eventListenerName, eventName, handler)
   }
 }
 
