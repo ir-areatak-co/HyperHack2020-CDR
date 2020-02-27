1b2b67ced3a76f58befca282be71a11a2f73096a
hnamazian <namazian87@gmail.com>
2020-02-27 12:57:46 +0330

    Initialize logging manager

---
+++ boot/logging.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const winston = require('winston')
+const { timestamp, printf } = winston.format
+
+/**
+ * Configure directory and format of logs for error, info and debug log levels
+ */
+module.exports = function () {
+  const errorFormatter = printf(error => {
+    let stack = ''
+    if (error.stack) {
+      stack = error.stack.replace(/[\r\n]+/g, '')
+    }
+
+    return `{"timestamp":"${error.timestamp}", "level":"${error.level}", "message":"${error.message}", "stack":"${stack}"}`
+  })
+
+  const consoleFormatter = printf(error => `${error.level}: ${error.message}`)
+
+  winston.add(
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs/exceptions.log'),
+      format: winston.format.combine(timestamp(), errorFormatter),
+      level: 'error'
+    })
+  )
+
+  winston.add(
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs/info.log'),
+      format: winston.format.combine(timestamp(), errorFormatter),
+      level: 'info'
+    })
+  )
+
+  winston.add(
+    new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        consoleFormatter
+      )
+    })
+  )
+
+  /**
+   * Log uncaught exceptions at error level
+   */
+  process.on('uncaughtException', ex => {
+    winston.error({ message: ex.message, stack: ex.stack })
+  })
+
+  /**
+   * Throw error at unhandled rejections
+   */
+  process.on('unhandledRejection', ex => {
+    throw new SyntaxError(ex)
+  })
+}
