6ce31fa8cbc3f5eb96818b6572284465a05ca6ea
hnamazian <namazian87@gmail.com>
2020-02-27 17:06:53 +0330

    Add serverCerts and exceptions at booting app + Fix bug at reading server port

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const winston = require('winston')
+const config = require('config')
 
 global.rootPath = __dirname
 
 const app = express()
+require('./boot/exceptions')()
 require('./boot/logging')()
 require('./boot/mkdir')()
 require('./boot/db')()
+require('./boot/serverCerts')()
 
-const port = process.env.server_port
+const port = config.get('server').port
 app.listen(port, () => {
   winston.debug(`Server started on port ${port}`)
 })
