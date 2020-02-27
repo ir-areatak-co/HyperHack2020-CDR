c459eaf23810337488aa74365c4fa3185655578f
hnamazian <namazian87@gmail.com>
2020-02-27 21:23:05 +0330

    Add HlfChannel class

--- utils/Blockchain/HlfChannel.js
+++ utils/Blockchain/HlfChannel.js
@@ -1,17 +1,11 @@
 class HlfChannel {
-  constructor (channelName, gateway) {
-    gateway.getClient().then(client => {
-      this.channel = client.getChannel(channelName)
-    })
-  }
-
-  async getChannel (channelName, gateway) {
+  static async getChannel (channelName, gateway) {
     const client = await gateway.getClient()
     const channel = client.getChannel(channelName)
     return channel
   }
 
-  async getPeers (channelName, gateway) {
+  static async getPeers (channelName, gateway) {
     const channel = await this.getChannel(channelName, gateway)
     const peers = channel.getPeers()
     return peers
