cd3c840350abe35465209159425a2a191204a65f
hnamazian <namazian87@gmail.com>
2020-02-28 15:20:17 +0330

    Add CallEvents DAO class

---
+++ DAO/callEvents.js
@@ -0,0 +1,55 @@
+const CallEvents = require('../models/CallEvents')
+
+module.exports = class CallEventsDao {
+  static async get (query) {
+    const result = CallEvents.findOne(query)
+    return result
+  }
+
+  static async getMany (query, limit, offset) {
+    const result = CallEvents.find(query)
+      .skip(offset)
+      .limit(limit)
+    return result
+  }
+
+  static async insert (data) {
+    try {
+      const callEvent = await CallEvents(data)
+      const result = await callEvent.save()
+      return result
+    } catch (ex) {
+      throw new Error(ex.errmsg)
+    }
+  }
+
+  static async update (data) {
+    try {
+      const callEvent = await CallEvents.findOne({ transactionId: data.transactionId })
+      if (callEvent) {
+        await callEvent.update(data)
+        return callEvent
+      }
+
+      return null
+    } catch (ex) {
+      throw new Error(ex.errmsg)
+    }
+  }
+
+  static async upsert (data) {
+    try {
+      let callEvent = await CallEvents.findOne({ transactionId: data.transactionId })
+      if (callEvent) {
+        await callEvent.update(data)
+        return callEvent
+      }
+
+      callEvent = await CallEvents(data)
+      const result = await callEvent.save()
+      return result
+    } catch (ex) {
+      throw new Error(ex.errmsg)
+    }
+  }
+}
