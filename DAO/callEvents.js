6094eabbc6214e0987cdf39c2bbf9a8499c71446
hnamazian <namazian87@gmail.com>
2020-02-28 20:57:23 +0330

    Add some new query methods

--- DAO/callEvents.js
+++ DAO/callEvents.js
@@ -1,19 +1,37 @@
 const CallEvents = require('../models/CallEvents')
 
 module.exports = class CallEventsDao {
-  static async get (query) {
+  static async get(query) {
     const result = CallEvents.findOne(query)
     return result
   }
 
-  static async getMany (query, limit, offset) {
+  static async getAll(query, fields) {
+    const result = CallEvents.find(query).select(fields)
+    return result
+  }
+
+  static async getMany(query, limit, offset) {
     const result = CallEvents.find(query)
       .skip(offset)
       .limit(limit)
     return result
   }
 
-  static async insert (data) {
+  static async getCounts(query) {
+    const result = CallEvents.find(query).count()
+    return result
+  }
+
+  static async getCountsByGroup(field) {
+    const result = CallEvents.aggregate([
+      { $match: { senderOperator: 'UsaOpServer' } },
+      { $group: { _id: 'senderOperator', total: { $sum: 1 } } }
+    ])
+    return result
+  }
+
+  static async insert(data) {
     try {
       const callEvent = await CallEvents(data)
       const result = await callEvent.save()
@@ -23,7 +41,7 @@ module.exports = class CallEventsDao {
     }
   }
 
-  static async update (data) {
+  static async update(data) {
     try {
       const callEvent = await CallEvents.findOne({ callId: data.callId })
       if (callEvent) {
@@ -37,7 +55,7 @@ module.exports = class CallEventsDao {
     }
   }
 
-  static async upsert (data) {
+  static async upsert(data) {
     try {
       let callEvent = await CallEvents.findOne({ callId: data.callId })
       if (callEvent) {
