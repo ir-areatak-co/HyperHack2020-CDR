dd3eb08b70cd14348ec19ff320c02f3edf9947c5
hnamazian <namazian87@gmail.com>
2020-02-28 15:19:59 +0330

    Add CallEvents model

---
+++ models/CallEvents.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const TransactionSchema = new mongoose.Schema({
+  transactionId: { type: String, required: true, unique: true, sparse: true },
+  blockNumber: { type: String, required: true },
+  status: { type: String, required: true }
+}, { _id: false })
+
+const callSchema = new mongoose.Schema({
+  callId: { type: String, required: true, unique: true },
+  senderOperator: { type: String, required: true },
+  receiverOperator: { type: String, required: true },
+  callerId: { type: String, required: true },
+  callReceiverId: { type: String, required: true },
+  startedAt: { type: String, required: true },
+  endedAt: { type: String, required: false },
+  status: { type: String, required: true },
+  startCallCreateTnx: { type: TransactionSchema, required: true },
+  startCallAcceptTnx: { type: TransactionSchema, required: false },
+  endCallTnx: { type: TransactionSchema, required: false }
+})
+
+const CallEvents = mongoose.model('CallEvents', callSchema)
+
+module.exports = CallEvents
