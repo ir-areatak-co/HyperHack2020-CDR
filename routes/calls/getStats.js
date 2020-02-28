adb34578716e6131ac4716ad008fd55401b775c7
hnamazian <namazian87@gmail.com>
2020-02-29 01:54:11 +0330

    Modify senderOperator names

--- routes/calls/getStats.js
+++ routes/calls/getStats.js
@@ -5,29 +5,25 @@ module.exports = async (req, resp) => {
   await CallEventsDao.getCounts({})
 
   // total calls (by each)
-  const totalUsaCalls = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServer' })
-  const totalIndiaCalls = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServer' })
+  const totalUsaCalls = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServerAdmin' })
+  const totalIndiaCalls = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServerAdmin' })
 
   // total start created calls (by each)
-  const totalUsaCallsStarted = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServer', status: 'START_CREATED' })
-  const totalIndiaCallsStarted = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServer', status: 'START_CREATED' })
+  const totalUsaCallsStarted = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServerAdmin', status: 'START_CREATED' })
+  const totalIndiaCallsStarted = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServerAdmin', status: 'START_CREATED' })
 
   // total start accepted calls (by each)
-  const totalUsaCallsStartAccepted = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServer', status: 'START_ACCEPTED' })
-  const totalIndiaCallsStartAccepted = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServer', status: 'START_ACCEPTED' })
+  const totalUsaCallsStartAccepted = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServerAdmin', status: 'START_ACCEPTED' })
+  const totalIndiaCallsStartAccepted = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServerAdmin', status: 'START_ACCEPTED' })
 
   // total ended calls (by each)
-  const totalUsaCallsEnded = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServer', status: 'ENDED' })
-  const totalIndiaCallsEnded = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServer', status: 'ENDED' })
+  const totalUsaCallsEnded = await CallEventsDao.getCounts({ senderOperator: 'UsaOpServerAdmin', status: 'ENDED' })
+  const totalIndiaCallsEnded = await CallEventsDao.getCounts({ senderOperator: 'IndiaOpServerAdmin', status: 'ENDED' })
 
   // total call duration (by each)
-  const usaDurations = await CallEventsDao.getAll({ senderOperator: 'UsaOpServer', status: 'ENDED' }, 'duration')
-  console.log(usaDurations);
-
+  const usaDurations = await CallEventsDao.getAll({ senderOperator: 'UsaOpServerAdmin', status: 'ENDED' }, 'duration')
   const usaDuration = usaDurations.reduce((a, b) => a.duration || 0 + b.duration || 0, 0)
-  const indiaDurations = await CallEventsDao.getAll({ senderOperator: 'IndiaOpServer', status: 'ENDED' }, 'duration')
-  console.log(indiaDurations);
-
+  const indiaDurations = await CallEventsDao.getAll({ senderOperator: 'IndiaOpServerAdmin', status: 'ENDED' }, 'duration')
   const indiaDuration = indiaDurations.reduce((a, b) => a.duration || 0 + b.duration || 0, 0)
 
   return resp.status(200).send({
